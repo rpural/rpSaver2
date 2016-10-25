68fb51ae3321518f324192410244a2a1621196c9
Nix, Robert P <Nix.Robert@mayo.edu>
2016-10-25 10:03:33 -0500

    Initial Commit

---
+++ rpSaver2/rpSaver2View.h
@@ -0,0 +1,13 @@
+//
+//  rpSaver2View.h
+//  rpSaver2
+//
+//  Created by Nix, Robert P. on 10/25/16.
+//  Copyright © 2016 Nix, Robert P. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface rpSaver2View : ScreenSaverView
+
+@end
---
+++ rpSaver2/rpSaver2View.m
@@ -0,0 +1,52 @@
+//
+//  rpSaver2View.m
+//  rpSaver2
+//
+//  Created by Nix, Robert P. on 10/25/16.
+//  Copyright © 2016 Nix, Robert P. All rights reserved.
+//
+
+#import "rpSaver2View.h"
+
+@implementation rpSaver2View
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
